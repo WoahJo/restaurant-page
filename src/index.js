609133f58ebdf3fd45cd530e9f3d33994811c3e6
WoahJo <ccnettacc@gmail.com>
2022-06-16 20:23:20 -0400

    Added styling to active tabs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import * as home from './home'
 import { menuItems } from './menu'
+// import { contact } from './contact'
 
 home.homepage()
 
+const menuToggle = () => {
+    home.tab1.style.color = "white";
+    home.tab2.style.color = "white";
+    home.tab3.style.color = "white";
+}
+
+home.title.addEventListener('click', () => {
+    home.homepage();
+})
+
+home.tab1.addEventListener('click', () => {
+    home.homepage();
+    menuToggle();
+    home.tab1.style.color = "orange";    
+});
+
 home.tab2.addEventListener('click', function(){
-    menuItems()
+    menuItems();
+    menuToggle();
+    home.tab2.style.color = "orange";
+});
+
+home.tab3.addEventListener('click', () => {
+    menuToggle();
+    home.tab3.style.color = "orange";
 });
